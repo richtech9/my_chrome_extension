2f8d8ab2ce5bd394d3dba8a9cffa31dcf40e86fe
richtech9 <richwon426@gmail.com>
2023-02-24 18:20:34 -0800

    chrome extension for fb remover

---
+++ fb-post-remover/background.js
@@ -0,0 +1,16 @@
+function setup() {
+	chrome.storage.local.get(["isSetupDone"]).then((result) => {
+		if (!Boolean(result["isSetupDone"])) {
+			chrome.storage.local.set({ option1: true });
+			chrome.storage.local.set({ option2: true });
+			chrome.storage.local.set({ option3: false });
+			chrome.storage.local.set({ option4: false });
+			chrome.storage.local.set({ option5: false });
+
+			chrome.storage.local.set({ isSetupDone: true });
+		}
+	});
+  	
+}
+
+setup();
---
+++ fb-post-remover/content.js
@@ -0,0 +1,79 @@
+let option1_state, option2_state, option3_state, option4_state, option5_state;
+chrome.storage.local.get(["option1"]).then((result) => {
+	option1_state = Boolean(result["option1"]);
+});
+
+chrome.storage.local.get(["option2"]).then((result) => {
+	option2_state = Boolean(result["option2"]);
+});
+
+chrome.storage.local.get(["option3"]).then((result) => {
+	option3_state = Boolean(result["option3"]);
+});
+
+chrome.storage.local.get(["option4"]).then((result) => {
+	option4_state = Boolean(result["option4"]);
+});
+
+chrome.storage.local.get(["option5"]).then((result) => {
+	option5_state = Boolean(result["option5"]);
+});
+
+const ms = Date.now();
+let classnames = ["x1lliihq"];
+fetch(
+	"https://raw.githubusercontent.com/richtech9/my_chrome_extension/main/config/class.json?dummy="+ms,
+)
+	.then((res) => res.json())
+	.then((data) => (classnames = data));
+
+function hide() {
+	const divs = document.querySelectorAll("div");
+
+	for (let i = 0; i < divs.length; i++) {
+		const div = divs[i];
+
+		if (
+			div.hasAttribute("data-pagelet") ||
+			classnames.includes(div.className)
+		) {
+			const spans = div.querySelectorAll("span");
+
+			for (let j = 0; j < spans.length; j++) {
+				const span = spans[j];
+
+				if (
+					(option1_state &&
+						span.textContent === "Recommended post") ||
+					(option2_state &&
+						span.textContent === "Suggested for you") ||
+					(option3_state &&
+						span.textContent === "Suggested Groups") ||
+					(option4_state &&
+						span.textContent === "Reels and short videos") ||
+					(option5_state &&
+						span.textContent.includes("shared a memory"))
+				) {
+					div.remove();
+					break;
+				}
+			}
+		}
+	}
+}
+
+window.onload = () => {
+	hide();
+};
+
+hide();
+
+var timer = null;
+document.addEventListener("scroll", (event) => {
+	if (timer === null) {
+		timer = setTimeout(() => {
+			hide();
+			timer = null;
+		}, 1 * 1000);
+	}
+});
